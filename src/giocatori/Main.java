0b82a688625ed833560e17c063cdca00de5967c4
vizzarro.21264 <luvizza9@gmail.com>
2024-10-07 13:32:11 +0200

    .

--- src/giocatori/Main.java
+++ src/giocatori/Main.java
@@ -6,31 +6,61 @@ public class Main {
     public static void main (String [] args){
         Scanner in=new Scanner(System.in);
 
-        Giocatore g1=new Giocatore();
+        Giocatore [] g = new Giocatore[100];
+        int scelta;
+
+
+
+        g[0] =new Giocatore();
         System.out.println("Inserisci il nome del primo giocatore");
-        g1.setgiocatore(in.nextLine());
+        g[0].setgiocatore(in.nextLine());
         System.out.println("Inserisci quanti gol ha segnato il primo giocatore");
-        g1.setgol(in.nextInt());
+        g[0].setgol(in.nextInt());
+        System.out.println("Il giocatore inserito è capitano");
+        g[0].setcapitano(in.nextBoolean());
         in.nextLine();
 
-        Giocatore g2=new Giocatore();
+        g[1] =new Giocatore();
         System.out.println("Inserisci il nome del secondo giocatore");
-        g2.setgiocatore(in.nextLine());
+        g[1].setgiocatore(in.nextLine());
         System.out.println("Inserisci quanti gol ha segnato il secondo giocatore");
-        g2.setgol(in.nextInt());
+        g[1].setgol(in.nextInt());
+        System.out.println("Il giocatore inserito è capitano");
+        g[1].setcapitano(in.nextBoolean());
         in.nextLine();
 
-        Giocatore g3=new Giocatore();
+        g[2] =new Giocatore();
         System.out.println("Inserisci il nome del terzo giocatore");
-        g3.setgiocatore(in.nextLine());
+        g[2].setgiocatore(in.nextLine());
         System.out.println("Inserisci quanti gol ha segnato il terzo giocatore");
-        g3.setgol(in.nextInt());
+        g[2].setgol(in.nextInt());
+        System.out.println("Il giocatore inserito è capitano");
+        g[2].setcapitano(in.nextBoolean());
         in.nextLine();
 
 
-        System.out.println("Giocatore 1:" +g1.getgiocatore() +";" +g1.getgol());
-        System.out.println("Giocatore 2:" +g2.getgiocatore() +";" +g2.getgol());
-        System.out.println("Giocatore 3:" +g3.getgiocatore() +";" +g3.getgol());
+        System.out.println("Cosa vuoi fare?");
+        scelta= in.nextInt();
+        switch (scelta){
+            case 1:
+                for(int i=3;i<=g.length;i++){
+
+
+                }
+                for(int i=0;i<g.length;i++){
+
+                }
+
+
+
+
+        }
+
+
+
+        System.out.println("Giocatore 1:" +g[0].getgiocatore() +";" +g[0].getgol()+";" +g[0].getcapitano());
+        System.out.println("Giocatore 2:" +g[1].getgiocatore() +";" +g[1].getgol()+";" +g[1].getcapitano());
+        System.out.println("Giocatore 3:" +g[2].getgiocatore() +";" +g[2].getgol()+";" +g[2].getcapitano());
     }
 
 }
