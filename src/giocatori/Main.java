42af4f1f65abd514cc98a2ec593041081f3f94b7
vizzarro.21264 <luvizza9@gmail.com>
2024-10-07 13:46:29 +0200

    .

--- src/giocatori/Main.java
+++ src/giocatori/Main.java
@@ -43,24 +43,40 @@ public class Main {
         scelta= in.nextInt();
         switch (scelta){
             case 1:
-                for(int i=3;i<=g.length;i++){
 
-
-                }
+            case 2:
                 for(int i=0;i<g.length;i++){
-
+                    System.out.println("Giocatore" +i +":" +g[i].getgiocatore() +";" +g[i].getgol()+";" +g[i].getcapitano());
                 }
 
+            case 3:
+                int i;
+                System.out.println("Che giocatore vuoi cambiare?");
+                i= in.nextInt();
+                System.out.println("Inserisci il nome del  giocatore");
+                g[i].setgiocatore(in.nextLine());
+                System.out.println("Inserisci quanti gol ha segnato il giocatore");
+                g[i].setgol(in.nextInt());
+                System.out.println("Il giocatore inserito è capitano");
+                g[i].setcapitano(in.nextBoolean());
 
+            case 4:
+                int j;
+                System.out.println("Che giocatore vuoi eliminare?");
+                j=in.nextInt();
+                g[j].setgiocatore("");
+                g[j].setgol(0);
+                g[j].setcapitano(false);
 
+            case 5:
+              for(i=0;i<g.length;i++){
+                if(g[i].getgol()<=5)
+                    System.out.println(g[i].getgiocatore());
+              }
+            case 6:
 
         }
 
-
-
-        System.out.println("Giocatore 1:" +g[0].getgiocatore() +";" +g[0].getgol()+";" +g[0].getcapitano());
-        System.out.println("Giocatore 2:" +g[1].getgiocatore() +";" +g[1].getgol()+";" +g[1].getcapitano());
-        System.out.println("Giocatore 3:" +g[2].getgiocatore() +";" +g[2].getgol()+";" +g[2].getcapitano());
     }
 
 }
